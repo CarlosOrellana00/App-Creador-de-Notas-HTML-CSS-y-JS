d4c46fb234d308d630cec8e87dc391efb4616316
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-06-05 22:45:32 -0400

    6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 const addBox = document.querySelector(".add-box"),
 popupBox = document.querySelector(".popup-box"),
-closeIcon = popupBox.querySelector("header i");
+closeIcon = popupBox.querySelector("header i"),
+titleTag = popupBox.querySelector("input"),
+descTag = popupBox.querySelector("textarea"),
+addBtn = popupBox.querySelector("button");
 
 addBox.addEventListener("click", () => {
   popupBox.classList.add("show");
@@ -8,4 +11,11 @@ addBox.addEventListener("click", () => {
 
 closeIcon.addEventListener("click", () => {
   popupBox.classList.remove("show");
+});
+
+addBtn.addEventListener("click", e => {
+  e.preventDefault();
+  let noteTitle = titleTag.value,
+  noteDesc = descTag.value;
+  console.log(noteTitle, noteDesc);
 });
