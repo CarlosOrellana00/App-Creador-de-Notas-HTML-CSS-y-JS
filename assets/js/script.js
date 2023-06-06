a5174f0a700912265dc6fdb05f2a87ca3a9291c3
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-06-05 23:48:56 -0400

    10

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,29 @@ closeIcon.addEventListener("click", () => {
   popupBox.classList.remove("show");
 });
 
+function showNotes(){
+  notes.forEach((note) => {
+    let liTag =
+    `<li class="details">
+      <div class="details">
+        <p>${note.title}</p>
+        <span>${note.description}</span>
+      </div>
+      <div class="bottom-content">
+        <span>${note.date}</span>
+        <div class="setting>
+          <i class="uil uil-ellipsis-h"></i>
+          <ul class="menu">
+            <li><i class="uil uil-pen"></i>Editar</li>
+            <li><i class="uil uil-trash"></i>Borrar</li>
+          </ul>
+        </div>
+      </div>
+    </li>`;
+    addBox.insertAdjacentHTML("afterend", liTag);
+  });
+}
+
 addBtn.addEventListener("click", e => {
   e.preventDefault();
   let noteTitle = titleTag.value,
@@ -39,5 +62,6 @@ addBtn.addEventListener("click", e => {
     notes.push(noteInfo); //agregando nueva nota
     localStorage.setItem("notes", JSON.stringify(notes));//guardando nota en localstorage
     closeIcon.click();
+    showNotes();
   }
 });
